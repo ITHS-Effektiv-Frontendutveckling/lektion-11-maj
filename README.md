1789a8205831e05990f261b247b13ccb2ca0f571
Christoffer Wallenberg <christoffer.wallenberg@zocom.se>
2021-05-11 11:45:39 +0200

    Added files

---
+++ README.md
@@ -0,0 +1,15 @@
+# Lektion 11 maj
+
+## Övningar
+
+Shopping cart: https://gist.github.com/zocom-christoffer-wallenberg/147c0782b5b069cf60ba0a2babbab372
+
+## Artiklar
+
+React dokumentation: https://reactjs.org/
+
+## Videor
+
+https://scrimba.com/learn/learnreact
+
+## Inspelningar
---
+++ react-functional-components-example/hello-world/src/App.js
@@ -0,0 +1,37 @@
+//import logo from './logo.svg';
+import './App.css';
+
+import TodoItem from './components/TodoItem';
+
+//Funktionella komponenter i React måste starta med en stor bokstav
+//Detta för att React ska veta att det är en komponent
+function App() {
+  const isDone = false;
+  let todos = [
+    { task: 'Köp kaffe', done: true, id: 1 },
+    { task: 'Köp kaka', done: false, id: 2 },
+    { task: 'Brygg kaffe', done: false, id: 3 },
+    { task: 'Drick kaffe', done: false, id: 4 }
+  ]
+
+  return (
+    <section>
+      <h1>Todo</h1>
+      <ul>
+        { /** Loopar ut med array-metoden map som returnerar en komponent för varje objekt i arrayen
+         * och skickar med varje objekt som props till TodoItem
+         */}
+        { todos.map((todo) => {
+          return <TodoItem task={ todo.task } done={ todo.done } key={ todo.id } />
+        }) }
+
+        {/* <TodoItem task='Köp kaffe' done={ true } />
+        <TodoItem task='Köp kaka' done={ isDone } />
+        <TodoItem task='Brygg kaffe' done={ isDone } />
+        <TodoItem task='Drick kaffe' done={ isDone } /> */}
+      </ul>
+    </section>
+  );
+}
+
+export default App;
